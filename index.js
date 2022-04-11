0eb8b4dadbcb6f5285f595eb81c364b0cd47b5df
Krstngtmn <astraete.mail@gmail.com>
2022-04-11 18:37:36 +0300

    New try
    Trying to commit again, last one didn't work

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const tasksRouter = require('./routes/tasksRouter');
-
-
+//dependencies required for the app
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+//render css files
 app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+//placeholders for added task
+let task = ["buy socks", "practise with nodejs"];
+//placeholders for removed task
+const complete = ["finish jquery"];
 
-app.use('/', tasksRouter);
+//post route for adding new task 
+app.post("/addtask", function(req, res) {
+    const newTask = req.body.newtask;
+    //add the new task from the post route
+    task.push(newTask);
+    res.redirect("/");
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.post("/removetask", function(req, res) {
+    const completeTask = req.body.check;
+    //check for the "typeof" the different completed task, then add into the complete task
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        //check if the completed task already exits in the task when checked, then remove it
+        task.splice(task.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (let i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    res.redirect("/");
+});
+
+const deleteProduct = ((req, res) => {
+  const id = Number(req.params.productID)
+  const index = products.findIndex(product => product.id === id)
+  products.splice(index,1)
+  res.status(200).json('Product deleted')
+})
+
+
+//render the ejs and display added task, completed task
+app.get("/", function(req, res) {
+    res.render("index", { task: task, complete: complete });
+});
+
+//set app to listen on port 3000
+app.listen(3000, function() {
+    console.log("server is running on port 3000");
 });
